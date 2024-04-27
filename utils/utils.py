3ba6af822155f88b5ee41f8a2af880ad49ced017
oecorrechag <oecorrechag@unal.edu.co>
2024-04-27 07:47:39 -0500

    The modification of a function was carried out, and an unnecessary module was removed

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@
 # Funcion tipo1
 def tipo1(df, variable):
     
-    '''Funcion ingresa un dataframe y una variable, retornara un grafico'''
+    '''Funcion ingresa un dataframe y una variable, retornara proporciones'''
     
     salida = df.loc[:,[variable]]
     salida.insert(1, 'contador', 1)
@@ -13,3 +13,23 @@ def tipo1(df, variable):
     salida = salida.sort_values(by=['proporcion'], ascending=False)
     
     return salida
+
+def tipo1B(df, variable):
+    """
+    Función que calcula y retorna las proporciones de una variable en un dataframe.
+
+    Parámetros:
+        df (pandas.DataFrame): El dataframe que contiene la variable.
+        variable (str): El nombre de la variable a analizar.
+
+    Retorna:
+        pandas.DataFrame: Un dataframe con las proporciones de la variable.
+    """
+
+    proporciones = df.groupby(variable).size().to_frame(name='contador')
+    proporciones['proporcion'] = proporciones['contador'] / len(df)
+    proporciones['proporcion'] = proporciones['proporcion'].apply(lambda x: f"{x:.5f}")
+    proporciones = proporciones.sort_values(by='proporcion', ascending=False)
+
+    mensaje = f"Proporciones de '{variable}':\n{proporciones}"
+    return mensaje
